0ca1c35123d6cee7d026597887babe7896c99a32
jerry <jerry.lu@mendrhub.com>
2020-05-31 15:19:56 +1000

    feat (packages): Add scss and semantics

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Container } from '@material-ui/core';
-import CardList from 'components/Cards/CardsList';
+import React, { useContext, useEffect } from 'react';
+import './App.scss';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import RegisterView from 'pages/RegisterLogin/register';
+import GlobalState from 'Context/GlobalState';
+import UserContext from 'Context/userContext';
 
 function App() {
+  const user = useContext(UserContext);
+  
   return (
-    <div className="App">
-      <Container>
-        <CardList />
-      </Container>
-    </div>
+    <GlobalState>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {}
+          </Route>
+          <Route>
+
+          </Route>
+        </Switch>
+        <div className="App">
+          <RegisterView />
+        </div>
+      </Router>
+    </GlobalState>
   );
 }
 
