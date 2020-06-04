517dd3f1227a6086f6543ecb1e8d1d854f543553
Justin <justinnhan@hotmail.com>
2020-06-05 00:52:49 +1000

    Enabled cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import { RegisterRoutes } from "./routes";
 
@@ -9,6 +10,7 @@ import requestLogger from 'middleware/request_logger';
 import setupDb from 'db';
 
 const app = express();
+app.use(cors());
 
 app.use(
   bodyParser.urlencoded({
