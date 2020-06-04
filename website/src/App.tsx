8d1e2612e673ebdc6c8063c9c6ec3b511d4d7c25
jerry <jerry.lu@mendrhub.com>
2020-06-05 00:19:18 +1000

    refactor (Imports): remove unused imports

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,23 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import "./style/App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalState from "Context/GlobalState";
-import UserContext from "Context/userContext";
 import LoginRegisterPage from "./pages/RegisterLogin/loginRegisterPage";
 
 function App() {
-  const user = useContext(UserContext);
-
   return (
     <GlobalState>
-      <Router>
-        {/* <Switch>
-          <Route path="/" exact>
+      <div className="App">
+        <Router>
+          {/* <Switch>
+            <Route path="/" exact>
             {}
-          </Route>
-          <Route></Route>
-        </Switch> */}
-        <div className="App">
-          <LoginRegisterPage />
-        </div>
-      </Router>
+            </Route>
+            <Route></Route>
+          </Switch> */}
+        </Router>
+        <LoginRegisterPage />
+      </div>
     </GlobalState>
   );
 }
