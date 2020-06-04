f7d521fedc31fcc9cf3fefe2a126f856699608c6
jerry <jerry.lu@mendrhub.com>
2020-06-05 00:15:15 +1000

    feat (Loader component): Add loaderComponent (blockui)

--- website/src/Context/GlobalState.tsx
+++ website/src/Context/GlobalState.tsx
@@ -1,33 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import UserContext from './userContext';
+import UserContext from "./userContext";
+import LoadingContext from "./loadingContext";
 
-const GlobalState = props => {
-    // authToken Provider
-    const [AuthToken, setAuthToken]  = useState("");
+const GlobalState = (props) => {
+  // authToken Provider
+  const [AuthToken, setAuthToken] = useState("");
+  // loading state provider
+  const [LoadingState, setLoadingState] = useState({
+    isLoading: false,
+    loadingMessage: "",
+  });
 
-    const isUserAuthenticated = () => {
-        if (AuthToken) {
-            return true;
-        }
-        return false;
+  // auth token provider functions
+  const isUserAuthenticated = () => {
+    if (AuthToken) {
+      return true;
     }
+    return false;
+  };
 
-    const updateAuthToken = (token: string) => {
-        setAuthToken(token);
-    }
+  const updateAuthToken = (token: string) => {
+    setAuthToken(token);
+  };
 
-    return (
-        <UserContext.Provider
-          value={{
-            authToken: AuthToken,
-            isUserAuthenticated,
-            updateAuthToken
-          }}
-        >
-          {props.children}
-        </UserContext.Provider>
-      );
+  // loading state provider functions
+  const toggleLoadingOn = (message: string) => {
+    setLoadingState({
+      isLoading: true,
+      loadingMessage: message,
+    });
+  };
+
+  const toggleLoadingOff = () => {
+    setLoadingState({
+      isLoading: false,
+      loadingMessage: "",
+    });
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        authToken: AuthToken,
+        isUserAuthenticated,
+        updateAuthToken,
+      }}
+    >
+      <LoadingContext.Provider
+        value={{
+          isLoading: LoadingState.isLoading,
+          loadingMessage: LoadingState.loadingMessage,
+          toggleLoadingOn: toggleLoadingOn,
+          toggleLoadingOff: toggleLoadingOff,
+        }}
+      >
+        {props.children}
+      </LoadingContext.Provider>
+    </UserContext.Provider>
+  );
 };
 
 export default GlobalState;
---
+++ website/src/Context/loadingContext.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createContext({
+    isLoading: false,
+    loadingMessage: "",
+    // update the AuthToken
+    toggleLoadingOn: (message: string) => {},
+    toggleLoadingOff: () => {}
+});
