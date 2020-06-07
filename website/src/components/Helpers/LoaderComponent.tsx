95bb761ba3075d94fd189d71982d55a23e8eb6af
Mxchaeltrxn <34886045+Mxchaeltrxn@users.noreply.github.com>
2020-06-07 21:36:55 +1000

    Feat/sponsor cards (#18)
    * Refactor folder and file structure and names
    
    * feat Create app layout and start working on nav bar
    
    * refactor Set up and apply eslint and prettier
    
    * Add some linting commands

--- website/src/components/Helpers/LoaderComponent.tsx
+++ website/src/components/Helpers/LoaderComponent.tsx
@@ -1,15 +1,15 @@
-import React, { useContext } from "react";
-import { Dimmer, Loader } from "semantic-ui-react";
-import LoadingContext from "../../Context/loadingContext";
+import React, { useContext } from 'react'
+import { Dimmer, Loader } from 'semantic-ui-react'
+import LoadingContext from '../../context/loadingContext'
 
 const LoaderComponent = () => {
-  const loading = useContext(LoadingContext);
+  const loading = useContext(LoadingContext)
 
   return (
     <Dimmer active={loading.isLoading}>
       <Loader>{loading.loadingMessage}</Loader>
     </Dimmer>
-  );
-};
+  )
+}
 
-export default LoaderComponent;
+export default LoaderComponent
